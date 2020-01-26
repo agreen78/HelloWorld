3daabd62a7dec172344f601cb4bb6c0feff818d2
agreen78 <andrewgreen78@gmail.com>
2020-01-26 13:40:24 -0500

    Version 2 of Hello World

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace HelloWorld
         {
             // Pause to display Hello World!
             Console.WriteLine("Press any key to continue...");
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! How much wood would a woodchuck chuck if a woodchuck could chuck wood?");
             Console.ReadKey(true);
         }
     }
